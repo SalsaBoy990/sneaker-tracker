4f4e9f3afd0b283836037b4d6d3e98763e67da47
Andras Gulacsi <guland@protonmail.com>
2020-06-19 01:49:13 +0200

    small mistake corrected - close-x btn

--- themes/cssgrid/js/side-collapse-form.js
+++ themes/cssgrid/js/side-collapse-form.js
@@ -1,30 +1,31 @@
-;jQuery(document).ready(function () {
-  var closeBtn = document.getElementById('close-side-form');
-  console.log(closeBtn);
+jQuery(document).ready(function () {
+  var closeBtn = document.getElementById("close-side-form");
+  var openBtn = document.querySelector(".sidebar-arrow");
+
   closeBtn.addEventListener("click", function () {
-    var sidebar = document.querySelector('.sidebar');
-    var collapse = document.querySelector('.collapse-form');
+    var sidebar = document.querySelector(".sidebar");
+    var collapse = document.querySelector(".collapse-form");
     // console.log(collapsable);
     sidebar.style.width = "119px";
-    collapse.style.display = 'none';
+    collapse.style.display = "none";
+    openBtn.src =
+      "http://sneaker-tracker.test/wp-content/themes/cssgrid/images/arrow_icon_open.png";
   });
 
-  var openBtn = document.querySelector('.sidebar-arrow');
   openBtn.addEventListener("click", function () {
-    var sidebar = document.querySelector('.sidebar');
-    var collapse = document.querySelector('.collapse-form');
+    var sidebar = document.querySelector(".sidebar");
+    var collapse = document.querySelector(".collapse-form");
 
-    if (collapse.style.display !== 'block') {
-       sidebar.style.width = "832px";
-       collapse.style.display = 'block';
-       openBtn.src="http://sneaker-tracker.test/wp-content/themes/cssgrid/images/arrow_icon_close.png"
-      } else if (collapse.style.display === 'block') {
-        sidebar.style.width = "119px";
-        collapse.style.display = 'none';
-        openBtn.src="http://sneaker-tracker.test/wp-content/themes/cssgrid/images/arrow_icon_open.png"
+    if (collapse.style.display !== "block") {
+      sidebar.style.width = "832px";
+      collapse.style.display = "block";
+      openBtn.src =
+        "http://sneaker-tracker.test/wp-content/themes/cssgrid/images/arrow_icon_close.png";
+    } else if (collapse.style.display === "block") {
+      sidebar.style.width = "119px";
+      collapse.style.display = "none";
+      openBtn.src =
+        "http://sneaker-tracker.test/wp-content/themes/cssgrid/images/arrow_icon_open.png";
     }
-
   });
-
-  
 });
